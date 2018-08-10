aa3ec549ced7a6c64e4fea53ea32411373da4456
KalagurAndrey <a.kalagur@robo.finance>
2018-08-10 17:14:43 +0700

    Оформление по psr2

--- index.php
+++ index.php
@@ -14,13 +14,13 @@ $shortopts .= "s";
 $shortopts .= "h";
 
 $longopts = array(
-	"input:",
-	"config:",
-	"output:",
-	"delimiter:",
-	"skip:",
-	"strict",
-	"help",
+    "input:",
+    "config:",
+    "output:",
+    "delimiter:",
+    "skip:",
+    "strict",
+    "help",
 );
 
 $options = getopt($shortopts, $longopts);
@@ -30,99 +30,104 @@ $config = $options['c'] ?? $options['config'] ?? null;
 $output = $options['o'] ?? $options['output'] ?? null;
 
 if (count($options) == 0) {
-	echo "Передайте все необходимые параметры. \n";
-	exit();
+     echo "Передайте все необходимые параметры. \n";
+     exit(1);
 }
 
 if (isset($options['h']) || isset($options['help'])) {
-	echo "Опции:
+     echo "Опции:
 -i|--input       путь до исходного файла
 -c|--config      путь до файла конфигурации
 -o|--output      путь до файла с результатом
 -d|--delimiter   задать разделитель (по умолчанию “,”)
---skip           пропускать модификацию первой строки исходного csv
---strict         проверять, что исходный файл содержит необходимое количество описанных в конфигурационном файле столбцов. При несоответствии выдавать ошибку.
+-k|--skip        пропускать модификацию первой строки исходного csv
+-s|--strict      проверять, что исходный файл содержит необходимое количество описанных 
+                 в конфигурационном файле  столбцов. При несоответствии выдавать ошибку.
 -h|--help        вывести справку \n";
-	exit();
+    exit(1);
 }
 
 
 if (isset($options['s']) || isset($options['strict'])) {
-	$strictOpen = fopen($input, 'r');
-		while ($stData = fgetcsv($strictOpen, 1000, ',')) {
-			$stNum = count($stData);
-		}
-			fclose($strictOpen);
-			if ($stNum == 4) {
-				echo "Исходный файл содержит необходимое количество столбцов. \n";
-			} else {
-					echo "Ошибка несоответствия столбцов во входном и конфигурационном файлах. \n";
-					exit();
-			}
+    $strictOpen = fopen($input, 'r');
+    while ($stData = fgetcsv($strictOpen, 1000, ',')) {
+        $stNum = count($stData);
+    }
+    fclose($strictOpen);
+    if ($stNum == 4) {
+         echo "Исходный файл содержит необходимое количество столбцов. \n";
+    } else {
+         echo "Ошибка несоответствия столбцов во входном и конфигурационном файлах. \n";
+         exit();
+    }
 }
 
 
-if ((isset($options['i']) || isset($options['input'])) && (isset($options['o']) || isset($options['output'])) && (isset($options['c']) || isset($options['config']))) {
+if ((isset($options['i']) || isset($options['input'])) && (isset($options['o']) || isset($options['output'])) &&
+	(isset($options['c']) || isset($options['config']))) {
 
 
-	if(!is_readable($input)) {
-		echo "Входной файл недоступен для чтения или его не существует. \n";
-		exit();
-	}
+if (!is_readable($input)) {
+     echo "Входной файл недоступен для чтения или его не существует. \n";
+     exit(1);
+}
 
-	if(!is_readable($config)) {
-		echo "Конфигурационный файл недоступен для чтения или его не существует. \n";
-		exit();
-	}
+if (!is_readable($config)) {
+     echo "Конфигурационный файл недоступен для чтения или его не существует. \n";
+     exit(1);
+}
 
-	if(!file_exists($config)) {
-		echo "Файл конфигурации отсутсвует или указан неверно. \n";
-		exit();
-	}
+if (!file_exists($config)) {
+     echo "Файл конфигурации отсутсвует или указан неверно. \n";
+     exit(1);
+}
 
-	if(!file_exists($input)) {
-		echo "Входной файл отсутсвует. \n";
-		exit();
-	}
-	$expansionIn = substr(strrchr($input, '.'), 1);
-	if($expansionIn != "csv") {
-		echo "Расширение входного файла не соответсвует условиям. \n";
-		exit();
-	}
+if(!file_exists($input)) {
+     echo "Входной файл отсутсвует. \n";
+     exit(1);
+}
+$expansionIn = substr(strrchr($input, '.'), 1);
+if ($expansionIn != "csv") {
+     echo "Расширение входного файла не соответсвует условиям. \n";
+     exit(1);
+}
 
-	$expansionConf = substr(strrchr($config, '.'), 1);
-	if($expansionConf != "php") {
-		echo "Расширение файла конфигурации не соответсвует условиям. \n";
-		exit();
-	}
+$expansionConf = substr(strrchr($config, '.'), 1);
+if ($expansionConf != "php") {
+     echo "Расширение файла конфигурации не соответсвует условиям. \n";
+     exit(1);
+}
 
-	$basenameConf = basename('conf.php');
-	if ($basenameConf != "conf.php") {
-		echo "Неправильное имя файла конфигурации. \n";
-	}
+$basenameConf = basename('conf.php');
+if ($basenameConf != "conf.php") {
+     echo "Неправильное имя файла конфигурации. \n";
+     exit(1);
+}
 
-	$arrFromConf = include $config;
+$arrFromConf = include $config;
 
 
-	$inputRead = fopen($input, "r") or die("Ошибка");
-	$outWrite = fopen($output, "w") or die("Ошибка");
+$inputRead = fopen($input, "r") or die("Ошибка");
+$outWrite = fopen($output, "w") or die("Ошибка");
 
-	if(!is_writable($output)) {
-		echo "Выходной файл недоступен для записи. \n";
-		exit();
-	}
-	$row = 1;
-	$encodingIn = mb_detect_encoding('input.csv');
-	$encodingOut = mb_detect_encoding('output.csv');
+if (!is_writable($output)) {
+echo "Выходной файл недоступен для записи. \n";
+     exit(1);
+}
 
-	for ($i = 0; $data = fgetcsv($inputRead, 1000, Delimiter($options)); $i++) {
+$row = 1;
+$encodingIn = mb_detect_encoding('input.csv');
+$encodingOut = mb_detect_encoding('output.csv');
 
-		if ($row == 1) {
-			if (isset($options['skip'])) {
-				$row++;
-				continue;
+for ($i = 0; $data = fgetcsv($inputRead, 1000, Delimiter($options)); $i++) {
 
-				}
+     if ($row == 1) {
+          if (isset($options['skip'])) {
+			 $row++;
+			 continue;
+
+
+	 }
 		}
 
 			foreach ($data as $key => $value) {
@@ -153,22 +158,16 @@ if ((isset($options['i']) || isset($options['input'])) && (isset($options['o']) 
 					}
 				}
 			}
-
-
 			fputcsv($outWrite, $dataFileOutput, Delimiter($options));
-
 	}
 
 	echo "Запись в файл успешно произведена. \n";
 	echo "Входной файл имеет кодировку: $encodingIn \n";
 	echo "Выходной файл имеет кодировку: $encodingOut \n";
-
-
 }
 
 fclose($inputRead);
 fclose($outWrite);
-
 
 
 
